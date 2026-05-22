a8cc0358def5f08aa2d775eb8b6495ecfcdd4b44
fssem1 <sara.miller@alaska.gov>
2026-05-22 12:54:04 -0800

    update data files

--- years_1976_on/AR1_model/code/model_data.R
+++ years_1976_on/AR1_model/code/model_data.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # data----
 brood <- read.csv("data/Situk_sockeye.csv", header = TRUE) %>% 
-  dplyr::select(S = spawn, R1 = recruit25, R2 = recruit50, R3 = recruit75) %>% 
+  dplyr::select(S = spawn, R = recruit50) %>% 
   mutate(n()) -> sr
 
 nyrs <- nrow(sr) #calculates the number of years of data
@@ -13,5 +13,5 @@ nyrs <- nrow(sr) #calculates the number of years of data
             #rk =1, ar1=1) 
 
 # Basic Ricker
-dat <- list(nyrs = nyrs, S = sr$S, R = sr$R2, rk = 1, ar1 = 1, d = 4) 
+dat <- list(nyrs = nyrs, S = sr$S, R = sr$R, rk = 1, ar1 = 1, d = 4) 
 # if running basic Ricker model, make sure that correct lnalpha.c chosen in the model.txt file
