ffc840e1a991f06f603dce424502b15e77195fc1
fssem1 <sara.miller@alaska.gov>
2026-05-29 12:24:17 -0800

    Changes Committed

---
+++ years_1976_on --save/AR1_model/code/model_data.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# data----
+brood <- read.csv("data/Situk_sockeye.csv", header = TRUE) %>% 
+  dplyr::select(S = spawn, R = recruit50) %>% 
+  mutate(n()) -> sr
+
+nyrs <- nrow(sr) #calculates the number of years of data
+
+# determine the dataset to run (only run one dat)
+# AR1 Ricker
+#dat <- list(nyrs = nyrs, S = sr$S, R = sr$R1, 
+            #rk =1, ar1=1) 
+
+# Basic Ricker
+dat <- list(nyrs = nyrs, S = sr$S, R = sr$R, rk = 1, ar1 = 1, d = 4) 
+# if running basic Ricker model, make sure that correct lnalpha.c chosen in the model.txt file
---
+++ years_1988_on --save/basic_Ricker_model/code/model_data.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+# data----
+read.csv("data/Situk_sockeye.csv", header = TRUE) %>%
+  mutate(year = as.numeric(year)) %>%
+  filter(year > 1987) %>% 
+  dplyr::select(S = spawn, R = recruit50) %>% 
+  mutate(n()) -> sr
+
+nyrs <- nrow(sr) #calculates the number of years of data
+
+# determine the dataset to run (only run one dat)
+# AR1 Ricker
+#dat <- list(nyrs = nyrs, S = sr$S, R = sr$R1, 
+            #rk =1, ar1=1) 
+
+# Basic Ricker
+dat <- list(nyrs = nyrs, S = sr$S, R = sr$R, rk = 1, ar1 = 0, d = 4) 
+# if running basic Ricker model, make sure that correct lnalpha.c chosen in the model.txt file
