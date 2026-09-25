041a0acb343d6a11decb9c1fdec3eb3390966017
Sara Miller <sara.miller@alaska.gov>
2026-09-24 20:28:38 -0700

    add code

--- code/2_regression_through_origin.R
+++ code/2_regression_through_origin.R
@@ -1,70 +1,27 @@
 # Situk Summary Figures
-# authors: Justin Priest
-# Last edited: August 2026
+# authors: Sara Miller
+# Last edited: September 2026
 
-
+# load libraries
 library(tidyverse)
-library(EGprocess)
-#remotes::install_github("commfish/adfggraph")
-library(adfggraph)
 
+Ahrn_data <- read_csv("data/Situk_sockeye_Ahrn_contrib.csv")
 
-rundata <- read_csv("data/Situk_sockeye_run.csv") %>%
-  rename(Year = yr, escapement = S)
+# fit regression through the origin
+y <- Ahrn_data$Ahrnklin_prop
+x <- Ahrn_data$age_0_diff
+model <- lm(y ~ x - 1)  # '- 1' removes intercept
 
-runharvdata <- read_csv("data/Situk_sockeye_harvestsources.csv") %>%
-  select(Year, commharv_total, subharv_total, sportharv_total, allharv_total) %>%
-  left_join(rundata %>% select(Year, escapement)) %>%
-  mutate(totalrun = allharv_total + escapement)
-runharvdata
+# view coefficients
+coef(model)
 
-runharvprop <- runharvdata %>% 
-  mutate(commharvrate  = commharv_total  / totalrun,
-         subharvrate   = subharv_total   / totalrun,
-         sportharvrate = sportharv_total / totalrun,
-         allharvrate   = allharv_total   / totalrun) %>% 
-  select(Year, commharvrate, subharvrate, sportharvrate, allharvrate) 
-runharvprop 
+# Calculate R^2 manually for regression through origin
+y_pred <- fitted(model)
+ss_res <- sum((y - y_pred)^2)       # Residual sum of squares
+ss_tot <- sum(y^2)                  # Total sum of squares (no mean subtraction)
+r2_origin <- 1 - ss_res / ss_tot
 
-
-plt_harvestrate <-runharvprop %>% 
-  pivot_longer(-Year, names_to = "harvestsource", values_to = "proportion") %>%
-  filter(harvestsource != "allharvrate") %>% 
-  # reorder so that they appear in plot with sport on top, comm on bottom
-  #  then rename them 
-  mutate(harvestsource = fct_relevel(harvestsource,"sportharvrate", 
-                           "subharvrate","commharvrate"),
-         # rename the values 
-         harvestsource = fct_recode(harvestsource,
-                             "Sport   "       = "sportharvrate",
-                             "Subsistence   " = "subharvrate",
-                             "Commercial   "  = "commharvrate")) %>%
-  ggplot(aes(x=Year, y = proportion, fill = harvestsource)) +
-  geom_col() + 
-  scale_x_continuous(breaks = seq(1980, 2025, 10),
-                     minor_breaks = seq(1985, 2025, 5),
-                     guide = guide_axis(minor.ticks = TRUE)) +
-  scale_y_continuous(labels = scales::percent,
-                     expand = expansion(mult = c(0.01, 0.05))) + #reduce gap at bottom
-  scale_fill_manual(values = c(
-      "Commercial   "  = "#0072B2",
-      "Subsistence   " = "#D55E00",
-      "Sport   "       = "#009E73")) +
-  # alt palette
-  # scale_fill_manual(values = c(
-  #   "Commercial   "  = "#6C88A4",
-  #   "Subsistence   " = "#C07A62",
-  #   "Sport   "       = "#8FA97A"
-  # )) +
-
-  labs(x = "", y = "Harvest Rate", fill = "Fishery") + 
-  guides(fill = guide_legend(nrow = 1, title.position = "top",
-                             byrow = TRUE)) +
-  theme_adfg(font_family = "Arial", font_size = 12, box = TRUE,
-             legend.position = "bottom",
-             legend.title.align = 0.5) # centered
-plt_harvestrate
-
-ggsave(plt_harvestrate, file = "output/harvestratesources.png", dpi = 300, 
-       width = 6, height = 4, units = "in")
+# Output results
+cat("Slope:", coef(model), "\n")
+cat("R^2 (through origin):", r2_origin, "\n")
 
